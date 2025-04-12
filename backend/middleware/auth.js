7a022aebd97cfadcc64698359673da8a485f04ba
Mehraj Ali Khan <96174538+Mehraj001@users.noreply.github.com>
2025-04-13 00:38:41 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,32 +1,3 @@
-// const jwt = require('jsonwebtoken');
-// const User2 = require('../models/User');
-
-// const auth = async (req, res, next) => {
-//   try {
-//     // Get token from header
-//     const token = req.header('Authorization').replace('Bearer ', '');
-    
-//     // Verifyoken of user
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
-  
-//     const user = await User2.findById(decoded.id);
-    
-//     if (!user) {
-//       throw new Error();
-//     }
-    
-//     // Add user to request object
-//     req.user = user;
-//     req.token = token;
-    
-//     next();
-//   } catch (error) {
-//     res.status(401).json({ message: 'Authentication required' });
-//   }
-// };
-
-// module.exports = auth; 
 
 const jwt = require('jsonwebtoken');
 const User2 = require('../models/User');
